03854bd44b0834483a09bec2be8398095f318b12
Nidhi Rohra <www.nidz16@gmail.com>
2025-03-09 15:01:57 +0530

    14

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -153,7 +153,7 @@ def send_error(error_message: str, block_id: str):
 
 @app.get("/test")
 async def test_route():
-    return {"message": "Let's see how it works!"}
+    return {"message": "Worksssss!!!!!"}
 
 
 if __name__ == "__main__":
